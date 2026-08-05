12c344c439186360f98e17f03baf27a7a0ab0b5a
selvaofficial1026-design <selvaofficial1026@gmail.com>
2026-08-05 19:49:55 +0530

    Remove bottle counts from product names

--- Aqua_cool_2/src/constants/data.js
+++ Aqua_cool_2/src/constants/data.js
@@ -3,9 +3,9 @@ export const WHATSAPP_NUMBER = '918825940239';
 export const products = [
   { id: 1, name: '20 Litre Can',   price: 35, unitText: '/ can', image: '/images/nivedha_water_20l_can_1785852899253.jpg',    badge: 'Best Seller' },
   { id: 2, name: '2 Litre Bottle', price: 20, unitText: '/ bottle', image: '/images/nivedha_water_2l_bottle_1785852909619.jpg',  badge: null },
-  { id: 3, name: '1 Litre Bottle (12 Bottles / Case)', price: 120, unitText: '/ case', image: '/images/nivedha_water_1l_bottle_1785852926112.jpg',  badge: 'Popular' },
-  { id: 4, name: '500 ml Bottle (24 Bottles / Case)',  price: 120, unitText: '/ case', image: '/images/nivedha_water_500ml_bottle_1785852949159.jpg', badge: null },
-  { id: 5, name: '300 ml Bottle (30 Bottles / Case)',  price: 165, unitText: '/ case', image: '/images/nivedha_water_300ml_bottle_1785852961551.jpg', badge: null },
+  { id: 3, name: '1 Litre Bottle (Case)', price: 120, unitText: '/ case', image: '/images/nivedha_water_1l_bottle_1785852926112.jpg',  badge: 'Popular' },
+  { id: 4, name: '500 ml Bottle (Case)',  price: 120, unitText: '/ case', image: '/images/nivedha_water_500ml_bottle_1785852949159.jpg', badge: null },
+  { id: 5, name: '300 ml Bottle (Case)',  price: 165, unitText: '/ case', image: '/images/nivedha_water_300ml_bottle_1785852961551.jpg', badge: null },
 ];
 
 export const features = [
